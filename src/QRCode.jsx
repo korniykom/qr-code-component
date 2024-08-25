f4ebadca9ae0ad6adec867188f6b0376cef9e5e2
Maksym Korniiko <korniykom@gmail.com>
2024-08-25 13:29:33 +0300

    fix: font size

--- src/QRCode.jsx
+++ src/QRCode.jsx
@@ -6,10 +6,10 @@ function QRCode() {
       <div className="mt-[16px] h-[288px] w-[288px] rounded-[10px] bg-blue-500">
         <img className="rounded-[20px]" src={image} />
       </div>
-      <p className="mx-[32px] mb-[16px] mt-[24px] text-xl font-bold text-slate-900">
+      <p className="mx-[32px] mb-[16px] mt-[24px] text-[22px] font-bold text-slate-900">
         Improve your front-end skills by building projects
       </p>
-      <p className="mx-[32px] text-sm text-slate-500">
+      <p className="mx-[32px] text-[15px] text-slate-500">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
