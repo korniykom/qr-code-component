25ccebaa08fd0a775923b1316f6c5b3b6ff7e621
Maksym Korniiko <korniykom@gmail.com>
2024-08-28 15:53:33 +0300

    fix: line height of text

--- src/QRCode.jsx
+++ src/QRCode.jsx
@@ -3,13 +3,13 @@ import image from "./images/image-qr-code.png";
 function QRCode() {
   return (
     <div className="flex h-[499px] w-[320px] flex-col items-center rounded-[20px] bg-white text-center drop-shadow-md">
-      <div className="mt-[16px] h-[288px] w-[288px] rounded-[10px] bg-blue-500">
-        <img className="rounded-[20px]" src={image} />
+      <div className="mt-[1rem] h-[288px] w-[288px] rounded-[10px] bg-blue-500">
+        <img className="rounded-[1.25rem]" src={image} />
       </div>
-      <p className="mx-[32px] mb-[16px] mt-[24px] text-xl font-bold text-slate-900">
+      <h1 className="mx-[32px] mb-[16px] mt-[24px] text-[1.375rem] leading-[1.65rem] font-bold text-slate-900">
         Improve your front-end skills by building projects
-      </p>
-      <p className="mx-[32px] text-sm text-slate-500">
+      </h1>
+      <p className="mx-[32px] leading-[1.3125rem] text-[0.9375rem] text-slate-500">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,11 @@ import QRCode from "./QRCode.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="w-dvh flex h-dvh items-center justify-center bg-slate-300">
+    <main
+      main
+      className="w-dvh flex h-dvh items-center justify-center bg-slate-300"
+    >
       <QRCode />
-    </div>
-  </StrictMode>,
+    </main>
+  </StrictMode>
 );
