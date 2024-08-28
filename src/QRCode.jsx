40e020aca9108a4edb14d6176d9f7336422440be
Maksym Korniiko <korniykom@gmail.com>
2024-08-28 15:55:45 +0300

    fix: add alt to img

--- src/QRCode.jsx
+++ src/QRCode.jsx
@@ -4,7 +4,7 @@ function QRCode() {
   return (
     <div className="flex h-[499px] w-[320px] flex-col items-center rounded-[20px] bg-white text-center drop-shadow-md">
       <div className="mt-[1rem] h-[288px] w-[288px] rounded-[10px] bg-blue-500">
-        <img className="rounded-[1.25rem]" src={image} />
+        <img alt="QR code" className="rounded-[1.25rem]" src={image} />
       </div>
       <h1 className="mx-[32px] mb-[16px] mt-[24px] text-[1.375rem] leading-[1.65rem] font-bold text-slate-900">
         Improve your front-end skills by building projects
